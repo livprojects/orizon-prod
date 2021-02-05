e30b28854913cb956c6f5c94bca1605d55fe2d71
livprojects <liv.audigane@gmail.com>
2021-02-05 23:49:03 +0100

    ssl3

--- app/database.js
+++ app/database.js
@@ -5,7 +5,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,{
 	dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
-        ssl: true
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     },
 	define: {
 		timestamps: false
